e00b696cdbf5d902b302c1b8a4f00b9ebc19df10
LB <lukbuc@zoho.com>
2018-02-01 18:58:40 +0100

    extend function draw

--- cellsnack/bubble.js
+++ cellsnack/bubble.js
@@ -1,31 +1,43 @@
 class Bubble {
-	constructor(x,y,r = 50){
-		this.x = x;
-		this.y = y;
-		this.r = r;
-		this.brightness = 0;
-	}
+  constructor(x, y, r = 50) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.brightness = 0;
+  }
 
-	changeColor(bright){
-		this.brightness = bright;
-	}
+  intersects(other) {
+    let d = dist(this.x, this.y, other.x, other.y);
+    return (d < this.r + other.r);
+  }
 
-	contains(px,py){
-		let d = dist(px,py,this.x,this.y);
-		if(d  < this.r){
-			return true;
+  changeColor(bright) {
+    this.brightness = bright;
+  }
+
+  contains(px, py) {
+    let d = dist(px, py, this.x, this.y);
+    if (d < this.r) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  move() {
+		if(this.x > 800 || this.y > 600){
+			this.x = this.x + random(-1, 0);
+	    this.y = this.y + random(-1, 0);
 		} else {
-			return false;
+			this.x = this.x + random(-1, 1);
+	    this.y = this.y + random(-1, 1);
 		}
-	}
+  }
 
-	move(){
-		this.x = this.x + random(-6,6);
-		this.y = this.y + random(-6,6);
-	}
-
-	show(){
-		fill(this.brightness,125);
-		ellipse(this.x,this.y,this.r *2);
-	}
+  show() {
+    stroke(255);
+    strokeWeight(4);
+    fill(this.brightness, 125);
+    ellipse(this.x, this.y, this.r * 2);
+  }
 }
--- cellsnack/sketch.js
+++ cellsnack/sketch.js
@@ -1,32 +1,31 @@
-let bubble1;
-let bubble2;
-let player1;
-
+let bubbles = [];
 
 function setup() {
-	createCanvas(600,400);
-
-	bubble1 = new Bubble(200,200);
-	bubble2 = new Bubble(300,200,10);
-	player1 = new Player(mouseX,mouseY,50)
+  createCanvas(800, 600);
+  for (let i = 0; i < 20; i++) {
+    let x = random(width);
+    let y = random(height);
+    let r = random(10, 30);
+    bubbles[i] = new Bubble(x, y, r);
+  }
 }
 
 function draw() {
-	background(255,255,0);
-	bubble1.show();
-	bubble1.move();
+  background(0);
 
-	bubble2.show();
-	bubble2.move();
-
-	player1.show();
-	player1.x = mouseX;
-	player1.y = mouseY;
-
-	var counter = 0;
-	if(player1.intersects(bubble1)){
-		//background(200,200,200);
-		counter = counter + 1;
-		player1.changeRadius(counter);
-	}
+  for (let b of bubbles) {
+    b.show();
+    b.move();
+    let overlapping = false;
+    for (let other of bubbles) {
+      if (b !== other && b.intersects(other)) {
+        overlapping = true;
+      }
+    }
+    if (overlapping) {
+      b.changeColor(255);
+    } else {
+      b.changeColor(0);
+    }
+  }
 }
